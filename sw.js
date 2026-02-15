9660b4ffab42b00bb674f5715fa92e6831599cbb
massiyousfi23-source <massi.yousfi23@gmail.com>
2026-02-15 21:53:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,15 @@
 
-const CACHE_NAME = 'emargement-v1.0.4';
-
-// Stratégie : Network First (on essaie le réseau, sinon le cache)
-// C'est plus sûr pour éviter les écrans blancs si un fichier manque au cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
-
-self.addEventListener('install', event => {
+// Fichier de nettoyage
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map((cacheName) => caches.delete(cacheName))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
